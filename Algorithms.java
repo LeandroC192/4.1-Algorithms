de10c5148847a7d72b8f51690eb452a2a7cd51b4
LeandroC192 <Leandroc16@nycstudents.net>
2024-11-27 16:49:04 +0000

    Started adding algorithms, incomplete

--- Algorithms.java
+++ Algorithms.java
@@ -9,7 +9,16 @@ public class Algorithms {
         f = new File("Numbers.txt");
         int odds = odds();
         int evens = evens();
+        int twoDigits = twoDigits();
+        int greater500 = greater500();
+       // int greatest = greatest();
+        int sum = sum();
         System.out.println(odds);
+        System.out.println(evens);
+        System.out.println(twoDigits);
+        System.out.println(greater500);
+        //System.out.println(greatest);
+        System.out.println(sum);
         s.close();
     }
 
@@ -21,5 +30,41 @@ public class Algorithms {
                 odds++;
         }
         return odds;
+    }
+    public static int evens() throws FileNotFoundException{
+        s = new Scanner(f);
+        int evens = 0;
+        while (s.hasNext()) {
+            if (s.nextInt() % 2 == 0)
+                evens++;
+        }
+        return evens;
+    }
+    public static int twoDigits() throws FileNotFoundException{
+        s = new Scanner(f);
+        int twoDigits = 0;
+        while (s.hasNext()) {
+            int current = s.nextInt();
+            if (current >= 10 && current <= 99)
+                twoDigits++;
+        }
+        return twoDigits;
+    }
+    public static int greater500() throws FileNotFoundException{
+        s = new Scanner(f);
+        int greater500 = 0;
+        while (s.hasNext()) {
+            if (s.nextInt() > 500)
+                greater500++;
+        }
+        return greater500;
+    }
+    public static int sum() throws FileNotFoundException{
+        s = new Scanner(f);
+        int sum = 0;
+        while (s.hasNext()) {
+              sum += s.nextInt();
+        }
+        return sum;
     }
 }
